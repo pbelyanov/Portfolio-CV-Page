72592d60046542f904461124eedaa23611b8b5a3
pbelyanov <p.belyanov@yahoo.com>
2022-12-12 13:41:43 +0200

    Added previous experience section

--- src/nav.js
+++ src/nav.js
@@ -4,13 +4,17 @@ import {
 import {
     homeView
 } from "./views/homeView.js";
+import {
+    prevExp
+} from "./views/prevExperience.js";
 
 export function nav(event) {
     const homeButton = document.getElementById('home');
     const logoButton = document.getElementById('logo-name').getElementsByTagName('a')[0];
     const aboutMeButton = document.getElementById('aboutMe');
+    const previousExpButton = document.getElementById('prevExp');
 
-
+    previousExpButton.addEventListener('click', prevExp)
     aboutMeButton.addEventListener('click', aboutMeView)
     logoButton.addEventListener('click', homeView);
     homeButton.addEventListener('click', homeView);
--- src/views/aboutMeView.js
+++ src/views/aboutMeView.js
@@ -1,16 +1,16 @@
 import {
-    navSelect
+  navSelect
 } from "./navLightUp.js"
 import {
-    html,
-    render
+  html,
+  render
 } from "../../node_modules/lit-html/lit-html.js"
 export function aboutMeView(event) {
-    document.body.style.backgroundImage = `url(../images/background-aboutMe.png)`
-    navSelect('aboutMe');
-    const main = document.getElementsByTagName('main')[0];
+  document.body.style.backgroundImage = `url(../images/background-aboutMe.png)`
+  navSelect('aboutMe');
+  const main = document.getElementsByTagName('main')[0];
 
-    const aboutMe = () => html `<section id="aboutMeSection">
+  const aboutMe = () => html `<section id="aboutMeSection">
     <h1>About Me</h1>
     <div id='mainContainer'>
       <div id="info">
@@ -25,10 +25,10 @@ export function aboutMeView(event) {
       <div>CSS</div>
       <div>REST</div>
       <div>JIRA</div>
-      <div></div>
+      <div>SPA</div>
       </div>
     </div>
   </section>`
 
-    render(aboutMe(), main)
+  render(aboutMe(), main)
 }
