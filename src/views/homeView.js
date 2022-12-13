b9efe61c839ef5cfc402140d381c0d380fb937b8
pbelyanov <p.belyanov@yahoo.com>
2022-12-13 16:50:55 +0200

    changes to scaling

--- src/views/homeView.js
+++ src/views/homeView.js
@@ -16,13 +16,14 @@ export function homeView(event) {
 
   const main = document.getElementsByTagName('main')[0];
   const homeViewTemplate = () => html `<section id="homePage">
-
+    <div id="homePageView">
     <h1>Hey! It's Petar Belaynov...</h1>
     <p>
         A beginner web developer with a sales & complaints handling background.
     </p>
     <button id="creator-journey">
     &lt;Check out my journey!&gt;</button>
+    </div>
   </section>`
 
   render(homeViewTemplate(), main)
