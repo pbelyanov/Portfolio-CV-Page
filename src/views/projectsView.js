2d7fca0a13cc27c751ea01ca732aaf4a40c689e5
pbelyanov <p.belyanov@yahoo.com>
2023-01-29 12:17:38 +0200

    minor changes

--- src/views/projectsView.js
+++ src/views/projectsView.js
@@ -1,75 +1,62 @@
-import {
-    navSelect
-} from "./navLightUp.js"
-import {
-    html,
-    render
-} from 'https://unpkg.com/lit-html?module'
-import {
-    weatherAPP
-} from "../weather_app/weather.js";
-import {
-    toDoApp
-} from "../to_do_app/toDoApp.js";
-import {
-    calc
-} from "../calculator_app/calc.js";
+import { navSelect } from "./navLightUp.js";
+import { html, render } from "https://unpkg.com/lit-html?module";
+import { weatherAPP } from "../weather_app/weather.js";
+import { toDoApp } from "../to_do_app/toDoApp.js";
+import { calc } from "../calculator_app/calc.js";
 
 export function projectsView(event) {
-    document.body.style.backgroundImage = `url(images/background-projects.png)`
-    navSelect('projects');
-    const main = document.getElementsByTagName('main')[0];
+  document.body.style.backgroundImage = `url(images/background-projects.png)`;
+  navSelect("projects");
+  const main = document.getElementsByTagName("main")[0];
 
-    const projects = () => html `
-    <section id='projectSection'>
-        <h1>PROJECTS</h1>
-        <div id="mainContainerProjects">
-            <div><button id="previous">&lt;</button></div>
-            <div id="img-box">
-                <div id="img1"><img src="images/weather-app-logo.png"></div>
-                <div id="img2"><img src="images/to-do-app.jpg"></div>
-                <div id="img3"><img src="images/calc-image.jpg"></div>
-            </div>
-            <div><button id="next">&gt</button></div>
-
+  const projects = () => html`
+    <section id="projectSection">
+      <h1>PROJECTS</h1>
+      <div id="mainContainerProjects">
+        <div><button id="previous">&lt;</button></div>
+        <div id="img-box">
+          <div id="img1"><img src="images/weather-app-logo.png" /></div>
+          <div id="img2"><img src="images/to-do-app.jpg" /></div>
+          <div id="img3"><img src="images/calc-image.jpg" /></div>
         </div>
+        <div><button id="next">&gt;</button></div>
+      </div>
     </section>
-    `
-    render(projects(), main)
+  `;
+  render(projects(), main);
 
-    let i = 0;
-    document.getElementsByTagName('img')[i].style.display = 'block'
+  let i = 0;
+  document.getElementsByTagName("img")[i].style.display = "block";
 
-    const previousButton = document.getElementById('previous');
-    const nexButton = document.getElementById('next');
+  const previousButton = document.getElementById("previous");
+  const nexButton = document.getElementById("next");
 
-    previousButton.addEventListener('click', previousPicture);
-    nexButton.addEventListener('click', nextPicture);
+  previousButton.addEventListener("click", previousPicture);
+  nexButton.addEventListener("click", nextPicture);
 
-    function previousPicture() {
-        document.getElementsByTagName('img')[i].style.display = 'none'
+  function previousPicture() {
+    document.getElementsByTagName("img")[i].style.display = "none";
 
-        i--
-        if (i < 0) {
-            i = 2
-        }
-
-        document.getElementsByTagName('img')[i].style.display = 'block'
+    i--;
+    if (i < 0) {
+      i = 2;
     }
 
-    function nextPicture() {
-        document.getElementsByTagName('img')[i].style.display = 'none'
+    document.getElementsByTagName("img")[i].style.display = "block";
+  }
 
-        i++
-        if (i >= 3) {
-            i = 0
-        }
+  function nextPicture() {
+    document.getElementsByTagName("img")[i].style.display = "none";
 
-        document.getElementsByTagName('img')[i].style.display = 'block'
+    i++;
+    if (i >= 3) {
+      i = 0;
     }
 
-    document.getElementById('img1').addEventListener('click', weatherAPP)
-    document.getElementById('img2').addEventListener('click', toDoApp)
-    document.getElementById('img3').addEventListener('click', calc)
+    document.getElementsByTagName("img")[i].style.display = "block";
+  }
 
+  document.getElementById("img1").addEventListener("click", weatherAPP);
+  document.getElementById("img2").addEventListener("click", toDoApp);
+  document.getElementById("img3").addEventListener("click", calc);
 }
--- src/weather_app/util.js
+++ src/weather_app/util.js
@@ -1,79 +1,79 @@
-import {
-    weatherAPP
-} from "./weather.js";
+import { weatherAPP } from "./weather.js";
 
 export function getPosition() {
-    const successCallback = (position) => {
-        sessionStorage.setItem('Latitude', position.coords.latitude);
-        sessionStorage.setItem('Longitude', position.coords.longitude);
-        return position;
-    };
+  const successCallback = (position) => {
+    sessionStorage.setItem("Latitude", position.coords.latitude);
+    sessionStorage.setItem("Longitude", position.coords.longitude);
+    return position;
+  };
 
-    const errorCallback = (error) => {
-        console.log(error);
-    };
+  const errorCallback = (error) => {
+    console.log(error);
+  };
 
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 }
 
 export async function getCity() {
-    const lat = sessionStorage.getItem('Latitude');
-    const lon = sessionStorage.getItem('Longitude');
+  const lat = sessionStorage.getItem("Latitude");
+  const lon = sessionStorage.getItem("Longitude");
 
-    const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=AIzaSyB0n8gzIoTQy5GhbGiWZfO_aszOqWcLxY8&language=english`)
-    const cityData = await response.json();
-    return cityData;
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=AIzaSyB0n8gzIoTQy5GhbGiWZfO_aszOqWcLxY8&language=english`
+  );
+  const cityData = await response.json();
+  return cityData;
 }
 
 export async function getWeather(event) {
-    const lat = sessionStorage.getItem('Latitude');
-    const lon = sessionStorage.getItem('Longitude');
-    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max&timezone=auto&daily=temperature_2m_min&daily=weathercode&current_weather=true`);
-    const data = await response.json();
+  const lat = sessionStorage.getItem("Latitude");
+  const lon = sessionStorage.getItem("Longitude");
+  const response = await fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max&timezone=auto&daily=temperature_2m_min&daily=weathercode&current_weather=true`
+  );
+  const data = await response.json();
 
-    const result = []
-    const forecast = {}
+  const result = [];
+  const forecast = {};
 
-    for (let i = 0; i < data.daily.time.length; i++) {
-        forecast[data.daily.time[i]] = {
-            minTemp: `${data.daily.temperature_2m_min[i]}&#176;`,
-            maxTemp: `${data.daily.temperature_2m_max[i]}&#176;`,
-            weatherCode: data.daily.weathercode[i]
-        }
-    }
+  for (let i = 0; i < data.daily.time.length; i++) {
+    forecast[data.daily.time[i]] = {
+      minTemp: `${data.daily.temperature_2m_min[i]}&#176;`,
+      maxTemp: `${data.daily.temperature_2m_max[i]}&#176;`,
+      weatherCode: data.daily.weathercode[i],
+    };
+  }
 
-    result.push(forecast)
-    result.push(data.current_weather)
-    return result
+  result.push(forecast);
+  result.push(data.current_weather);
+  return result;
 }
 
 export function searchLocation(event) {
-    const input = document.getElementById("pac-input");
-    const searchBox = new google.maps.places.SearchBox(input);
+  const input = document.getElementById("pac-input");
+  const searchBox = new google.maps.places.SearchBox(input);
 
-    searchBox.addListener("places_changed", () => {
-        const places = searchBox.getPlaces();
+  searchBox.addListener("places_changed", () => {
+    const places = searchBox.getPlaces();
 
-        if (places.length == 0) {
-            return;
-        }
-        const bounds = new google.maps.LatLngBounds();
+    if (places.length == 0) {
+      return;
+    }
+    const bounds = new google.maps.LatLngBounds();
 
-        places.forEach((place) => {
-            if (!place.geometry || !place.geometry.location) {
-                console.log("Returned place contains no geometry");
-                return;
-            }
-            if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
-            } else {
-                bounds.extend(place.geometry.location);
-            }
-        });
-        sessionStorage.setItem('Latitude', (bounds.Wa.hi + bounds.Wa.lo) / 2);
-        sessionStorage.setItem('Longitude', (bounds.Ia.hi + bounds.Ia.lo) / 2);
-        weatherAPP();
+    places.forEach((place) => {
+      if (!place.geometry || !place.geometry.location) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
     });
+    sessionStorage.setItem("Latitude", (bounds.Wa.hi + bounds.Wa.lo) / 2);
+    sessionStorage.setItem("Longitude", (bounds.Ia.hi + bounds.Ia.lo) / 2);
+  });
 }
